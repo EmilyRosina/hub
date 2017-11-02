4c079d85072ec98e8904a3d5875782fae69c30ca
Emily Carey <emilyrosinacarey@gmail.com>
2017-11-02 22:32:44 +0000

    adding examples i like

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -12,6 +12,7 @@
               <p>examples i like...</p>
               <img src="https://reflect.io/assets/blog/github-screenshot-350443090d37bcbe5d838673f90149df0f1defdad08be88eb16438901411003c.png"/>
               <img src="https://purecss.io/img/layouts/blog@2x.jpg"/>
+              <img src="https://1.bp.blogspot.com/-paSFxN2dtwk/WLyUzSFLjtI/AAAAAAAAAEA/03kct5t9JMMN7KiPntcCe6VoLHfBGtGLwCLcB/s1600/en_Notable.jpg"/>
             </sl-col>
           </sl-row>
         </sl-container>
