f77bfbd324243c9e233991f01560b1f3df5b412e
Emily Carey <emilyrosinacarey@gmail.com>
2017-11-08 20:46:28 +0000

    breakpoints in getters and sidebar breakpoints

--- src/components/layout/sidebar.vue
+++ src/components/layout/sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <sl-col col="3" id="sidebar">
+  <sl-col col="12" md="4" lg="3" xl="2" id="sidebar">
     <p>// sidebar //</p>
     <p>...logo to go here...</p>
     <p>...navigation to go here...</p>
@@ -11,11 +11,24 @@
   import slRow from 'shoelace/row.vue'
   import slCol from 'shoelace/col.vue'
 
+  // let screen = {
+  //   width: window.innerWidth,
+  //   height: window.innerHeight
+  // }
+
   export default {
     components: {
       slContainer,
       slRow,
       slCol
+    },
+    mounted () {
+      console.log(this)
+    },
+    computed: {
+      screenSize () {
+        return this.$el.clientWidth
+      }
     }
   }
 </script>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,22 @@
-export default {
+import breakpoints from './utility/breakpoints'
+// import actions from './actions'
+// import mutations from './mutations'
 
+export default {
+  breakpoint (state) {
+    let breakpoint = ''
+    do {
+      for (let bp in breakpoints) {
+        let min = breakpoints[bp][0]
+        let max = breakpoints[bp][1]
+        if (state.width >= min && state.width <= max) {
+          breakpoint = bp
+        }
+      }
+    } while (breakpoint === '')
+    return breakpoint
+  }
+  // get_breakpoint_byWidth (state) => {(width) => {
+  //   return width
+  // }
 }
