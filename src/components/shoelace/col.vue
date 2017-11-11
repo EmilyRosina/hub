c0e01bca3e8b53a78e09cbdf900f4143b63912da
Emily Carey <emilyrosinacarey@gmail.com>
2017-11-11 21:13:14 +0000

    added more props to sl-col

--- src/components/shoelace/col.vue
+++ src/components/shoelace/col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['col', classes]">
+  <div :class="['col', appliedClasses]">
     <slot></slot>
   </div>
 </template>
@@ -51,22 +51,72 @@
         type: String,
         default: '',
         required: false
+      },
+      flex: {
+        type: Boolean,
+        default: false,
+        required: false
+      },
+      grow: {
+        type: Number,
+        default: 0,
+        required: false
+      },
+      align: {
+        type: String,
+        default: 'start',
+        required: false
+      },
+      justify: {
+        type: String,
+        default: 'center',
+        required: false
+      }
+    },
+    data () {
+      return {
+        classes: {
+          shoelace: {
+            breakpoints: ['xs', 'sm', 'md', 'lg', 'xl'],
+            utility: ['col', 'hide', 'order', 'offset']
+          },
+          flex: ['align', 'justify', 'grow']
+        }
       }
     },
     computed: {
-      classes () {
+      shoelaceClasses () {
         let props = this._props
-        let classes = Object.keys(props).filter(prop => {
+        let shoelaceClasses = {}
+        shoelaceClasses.breakpoints = this.classes.shoelace.breakpoints.filter(prop => {
           return props[prop].length > 0
         })
+        shoelaceClasses.utility = this.classes.shoelace.utility.filter(prop => {
+          return props[prop].length > 0
+        })
+        return shoelaceClasses
+      },
+      flexClasses () {
+        let props = this._props
+        let flexClasses = this.classes.flex.filter(prop => {
+          return props[prop]
+        })
+        return flexClasses
+      },
+      appliedClasses () {
+        let props = this._props
         let list = []
-        let breakpoints = ['xs', 'sm', 'md', 'lg', 'xl']
-        classes.forEach((x) => {
-          if (breakpoints.includes(x)) {
-            list.push(`col-${x}-${props[x]}`)
-          } else {
-            list.push(`${x}-${props[x]}`)
-          }
+        this.shoelaceClasses.breakpoints.forEach(x => {
+          list.push(`col-${x}-${props[x]}`)
+        })
+        this.shoelaceClasses.utility.forEach(x => {
+          list.push(`${x}-${props[x]}`)
+        })
+        if (props.flex) {
+          list.push('col-flex')
+        }
+        this.flexClasses.forEach(x => {
+          list.push(`col-flex-${x}-${props[x].toString()}`)
         })
         return list.join(' ')
       }
@@ -74,8 +124,51 @@
   }
 </script>
 
-<style>
-  .row {
+<style lang="scss" scoped>
+  .col-flex {
+    display: flex;
+    flex-direction: column;
+    &-align {
+      &-start {
+        align-items: flex-start;
+      }
+      &-center {
+        align-items: center;
+      }
+      &-end {
+        align-items: flex-end;
+      }
+    }
+    &-justify {
+      &-start {
+        justify-content: flex-start;
+      }
+      &-center {
+        justify-content: center;
+      }
+      &-end {
+        justify-content: flex-end;
+      }
+    }
+    &-grow {
+      &-1 {
+        flex: 1 0 auto;
+      }
+      &-2 {
+        flex: 1 0 auto;
+      }
+      &-3 {
+        flex: 1 0 auto;
+      }
+      &-4 {
+        flex: 1 0 auto;
+      }
+      &-5 {
+        flex: 1 0 auto;
+      }
+    }
+  }
+  /* .row {
     background-color: #ccc;
   }
   .col {
@@ -84,5 +177,5 @@
     color: white;
     text-align: center;
     border: 2px solid white;
-  }
+  } */
 </style>
