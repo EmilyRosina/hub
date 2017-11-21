1232fb708d4a6c9f68d00ed2afd26cb451f7b2aa
Emily Carey <emilyrosinacarey@gmail.com>
2017-11-21 18:26:40 +0000

    added route for blog

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/views/Welcome'
+import Blog from '@/views/Blog'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/blog',
+      name: 'Blog',
+      component: Blog
     }
   ]
 })
