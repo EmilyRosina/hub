a5b590dcaa1acad061ced305bf0097dd146bd054
Emily Carey <emilyrosinacarey@gmail.com>
2019-03-04 00:23:11 +0000

    add prettier and amend eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,22 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb'],
   rules: {
+    semi: 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': 'off',
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never',
-    }],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never',
+      },
+    ],
+    'arrow-parens': ['warn', 'always'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'implicit-arrow-linebreak': ['error', 'beside'],
   },
   parserOptions: {
     parser: 'babel-eslint',
